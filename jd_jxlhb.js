844219399c57f3611623ae62df54f2b857d170b4
zhangshixiang89 <76580524+zhangshixiang89@users.noreply.github.com>
2021-11-15 09:54:41 +0800

    Update jd_jxlhb.js

--- jd_jxlhb.js
+++ jd_jxlhb.js
@@ -32,7 +32,7 @@ if ($.isNode()) {
 } else {
   cookiesArr = [$.getdata("CookieJD"), $.getdata("CookieJD2"), ...$.toObj($.getdata("CookiesJD") || "[]").map((item) => item.cookie)].filter((item) => !!item);
 }
-$.packetIdArr = [{"strUserPin":"LJD9tXHjqNMZIgN4ckZRxInL63Zg8vyeMwujE3dllfLCgZu354Ygen67paPI6P56","userName":"208332482-829664"},{"strUserPin":"LJD9tXHjqNMZIgN4ckZRxIvmIW5KApxNiL2yn0qS7jZmngJLr_w6xqP497fGFB-v","userName":"jd_6a8118a918455"}];
+$.packetIdArr = [{"strUserPin":"LafOJp9LOI9FWoXzIhViG4nL63Zg8vyeMwujE3dllfLCgZu354Ygen67paPI6P56","userName":"208332482-829664"},{"strUserPin":"LafOJp9LOI9FWoXzIhViG4vmIW5KApxNiL2yn0qS7jZmngJLr_w6xqP497fGFB-v","userName":"jd_6a8118a918455"}];
 $.activeId = '525597';
 const BASE_URL = 'https://m.jingxi.com/cubeactive/steprewardv3'
 !(async () => {
